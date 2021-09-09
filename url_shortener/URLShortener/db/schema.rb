9b3cbc9a51027f5f5c26ded457776675e5f1c295
gtsimis <gt2186@gmail.com>
2021-09-09 16:36:11 -0400

    Finished User model

--- url_shortener/URLShortener/db/schema.rb
+++ url_shortener/URLShortener/db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2021_09_09_192823) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
